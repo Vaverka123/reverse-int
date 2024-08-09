ee61f3e1e5f828db99e33d687295512556e0e84f
Vaverka123 <maslova.vera.2019@yandex.by>
2024-08-09 15:21:35 +0200

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (String(n).length === 1) {
+        return n;
+    }
+
+    return Number(
+        String(n)
+            .split("")
+            .reverse()
+            .filter((el) => Number.isInteger(+el))
+            .join("")
+    );
+};
